e913b23356bac1356f2875a67dd3423f2259a23d
blakewebb15 <89742731+blakewebb15@users.noreply.github.com>
2021-11-03 16:13:00 -0400

    Reverting

--- app/public/api/assignments/index.php
+++ app/public/api/assignments/index.php
@@ -7,8 +7,13 @@ $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
 <<<<<<< HEAD
+<<<<<<< HEAD
 $sql = 'SELECT title, field, gDate, time from game';
 
+=======
+$sql = 'SELECT g.title, g.field, g.gDate, g.time, CONCAT(r.fName," ",r.lName) as ref FROM game as g, referee as r, 
+assignment as a WHERE a.refID = r.refID and a.gameID = g.gameID';
+>>>>>>> parent of 1816b16 (Adding more code)
 =======
 $sql = 'SELECT g.title, g.field, g.gDate, g.time, CONCAT(r.fName," ",r.lName) as ref FROM game as g, referee as r, 
 assignment as a WHERE a.refID = r.refID and a.gameID = g.gameID';
