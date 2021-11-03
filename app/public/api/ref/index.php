2837235fd73375f69f378ae2fafa73e03a7069bc
niz10500 <niz@iu.edu>
2021-11-03 12:34:54 -0400

    referee
    referee

--- app/public/api/ref/index.php
+++ app/public/api/ref/index.php
@@ -6,22 +6,18 @@ require 'class/DbConnection.php';
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM student';
+$sql = 'SELECT * FROM referee';
 $vars = [];
 
-// if (isset($_GET['student'])) {
-//   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM student WHERE studentID = ?';
-//   $vars = [ $_GET['student'] ];
-// }
+
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$students = $stmt->fetchAll();
+$referees = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($students, JSON_PRETTY_PRINT);
+$json = json_encode($referees, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
