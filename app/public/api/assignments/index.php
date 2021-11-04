62ddfed0e9e5fa2bcb1de7ef6850f936032887a8
blakewebb15 <89742731+blakewebb15@users.noreply.github.com>
2021-11-03 20:14:41 -0400

    Games page works!
    Can view game assignments and add new games. Still need to put edit game functionality

--- app/public/api/assignments/index.php
+++ app/public/api/assignments/index.php
@@ -1,38 +1,30 @@
 <?php
-// require 'common.php';
 require 'class/DbConnection.php';
 
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
 // Step 2: Create & run the query
-<<<<<<< HEAD
-<<<<<<< HEAD
-$sql = 'SELECT title, field, gDate, time from game';
-
-=======
-$sql = 'SELECT g.title, g.field, g.gDate, g.time, CONCAT(r.fName," ",r.lName) as ref FROM game as g, referee as r, 
-assignment as a WHERE a.refID = r.refID and a.gameID = g.gameID';
->>>>>>> parent of 1816b16 (Adding more code)
-=======
-$sql = 'SELECT g.title, g.field, g.gDate, g.time, CONCAT(r.fName," ",r.lName) as ref FROM game as g, referee as r, 
-assignment as a WHERE a.refID = r.refID and a.gameID = g.gameID';
->>>>>>> parent of 1816b16 (Adding more code)
+$sql = 'SELECT * FROM assignment';
 $vars = [];
 
-// if (isset($_GET['student'])) {
-//   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM student WHERE studentID = ?';
-//   $vars = [ $_GET['student'] ];
-// }
+if (isset($_GET['game'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT a.assignmentID, a.gameID, r.fName, r.lName, a.status FROM assignment as a, referee as r WHERE a.refID = r.refID and a.gameID = ?';
+
+  //NOT THIS WAY
+  // $sql = 'SELECT * FROM offer WHERE studentId = ' . $_GET['student'];
+
+  $vars = [ $_GET['game'] ];
+}
 
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
-$games = $stmt->fetchAll();
+$offers = $stmt->fetchAll();
 
 // Step 3: Convert to JSON
-$json = json_encode($games, JSON_PRETTY_PRINT);
+$json = json_encode($offers, JSON_PRETTY_PRINT);
 
 // Step 4: Output
 header('Content-Type: application/json');
--- app/public/js/game.js
+++ app/public/js/game.js
@@ -3,7 +3,10 @@ const gameApp = {
       return {
           games: [],
           gameForm: {},
-          selectedGame: null
+          selectedGame: null,
+          assign: [],
+          refs: [],
+          refForm: {}
         
         }
     },
@@ -18,6 +21,8 @@ const gameApp = {
                 return;
             }
             this.selectedGame = g;
+            this.fetchAssignData(g);
+            this.fetchRefData();
             // this.games = [];
             // this.fetchOfferData(this.selectedStudent);
         },
@@ -31,6 +36,82 @@ const gameApp = {
             .catch( (err) => {
                 console.error(err);
             })
+        },
+        fetchAssignData(s) {
+            console.log("Fetching offer data for ", s);
+            fetch('/api/assignments/?game=' + s.gameID)
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.assign = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        },
+        fetchRefData() {
+            fetch('/api/ref')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                this.refs = responseJson;
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        },
+        postNewAssign(evt) {
+            this.refForm.gameID = this.selectedGame.gameID;        
+            
+            console.log("Creating!", this.refForm);
+    
+            fetch('api/assignments/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.refForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.assign = json;
+                
+                // reset the form
+              })
+              .catch( err => {
+                alert("Something went horribly wrong.");
+              });
+          },
+        postDeleteAssign(o) {  
+            if ( !confirm("Are you sure you want to delete the aassignment from " + o.fName + "?") ) {
+                return;
+            }  
+            
+            console.log("Delete!", o);
+    
+            fetch('api/assignments/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.assign= json;
+                
+                
+              });
         },
         postNewGame(evt) {  
             fetch('api/game/create.php', {
